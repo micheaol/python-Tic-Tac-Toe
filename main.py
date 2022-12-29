80a74874e3ade90ab8b0eff8ddc95768074a265d
Michael <micheaol80@gmail.com>
2022-12-29 15:50:53 +0100

    Add status code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -48,16 +48,25 @@ def get_post():
     return { "data": my_post}
 
 
-@app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(id)
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_post[len(my_post) - 1]
     return {"data": post}
 
 
-@app.post("/posts")
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post id: {id} does not exist"}
+    return {"data": post}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     dict_post = post.dict()
     dict_post["id"] = randrange(0, 100)
     my_post.append(dict_post)
-    print(my_post)
     return {"data": dict_post}

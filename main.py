56bf303a2f50fe4983ef2f590accc744d34922e7
Michael <micheaol80@gmail.com>
2022-12-29 07:53:45 +0100

    Add optional field to the api

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int] = None
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my python api"}
 
 
-@app.get("/user")
-def get_user():
-    return {"message": "You are the user"}
-
-
 @app.post("/post")
-def create_post(payLoad: dict = Body(...)):
-    return payLoad
+def create_post(new_post: Post):
+    return new_post

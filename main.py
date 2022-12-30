ae83cb3b3c16e09f5ed62da37234bddcfb68f7dc
Michael <micheaol80@gmail.com>
2022-12-30 09:04:19 +0100

    Add update route to the api

--- main.py
+++ main.py
@@ -75,3 +75,15 @@ def delete_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     my_post.pop(index)
     return {"data": "post deleted successfully"}
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post_dict = post.dict()
+
+    post_dict["id"] = id
+    my_post[index] = post_dict
+    return {"data": post_dict}

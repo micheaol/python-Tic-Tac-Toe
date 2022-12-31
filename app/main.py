a74776e51a5be76bf870026dd9f4b0bd4ed9004a
Michael <micheaol80@gmail.com>
2022-12-31 19:56:05 +0100

    Add logic and sql to update post

--- app/main.py
+++ app/main.py
@@ -70,7 +70,9 @@ def get_post():
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
+    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     return {"data": post}
@@ -90,20 +92,27 @@ def create_post(post: Post):
 
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index(id)
-    if index == None:
+
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    my_post.pop(index)
-    return {"data": "post deleted successfully"}
+
+    return {"data": "Post has been deleted successfully"}
+
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index(id)
 
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id))))
+
+    updted_post = cursor.fetchone()
+    conn.commit()
+    
+    if updted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    post_dict = post.dict()
 
-    post_dict["id"] = id
-    my_post[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updted_post}

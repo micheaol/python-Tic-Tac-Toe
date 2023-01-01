37ee7fc88cd29e5c0adcc2dc58f37fe818fa6229
Michael <micheaol80@gmail.com>
2023-01-01 11:07:15 +0100

    Add more fields

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
@@ -12,9 +12,11 @@ class PostCreate(PostBase):
 
 
 class Post(BaseModel):
+    id: int
     title: str
     content: str
     published: bool
+    created_at: datetime
 
     class Config:
         orm_mode = True

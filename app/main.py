d300fd27927b33f1acee14d093d49f9b9d6c41f1
Michael <micheaol80@gmail.com>
2023-01-01 08:17:16 +0100

    Update the routes with ORM

--- app/main.py
+++ app/main.py
@@ -82,9 +82,11 @@ def get_post(db: Session = Depends(get_db)):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
+    # post = cursor.fetchone()
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
@@ -92,40 +94,55 @@ def get_post(id: int):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    # dict_post = post.dict()
-    # dict_post["id"] = randrange(0, 100)
-    # my_post.append(dict_post)
-     new_post = cursor.fetchone()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    #  cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
 
-     conn.commit()
+    #  new_post = cursor.fetchone()
+    #  conn.commit()
+
+
+     new_post = models.Post(title=post.title, content=post.content, published=post.published)
+
+     db.add(new_post)
+     db.commit()
+     db.refresh(new_post)
+
      return {"data": new_post}
 
 
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
 
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
 
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
     
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
 
+    post.delete(synchronize_session=False)
+
+    db.commit()
     return {"data": "Post has been deleted successfully"}
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id))))
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id))))
 
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    if updated_post == None:
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
 
-    return {"data": updated_post}
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    
+    return {"data": post_query.first()}
